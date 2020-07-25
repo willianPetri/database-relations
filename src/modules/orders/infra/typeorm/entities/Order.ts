1e9106f71a8578b21cfbd363ba81200dc6b085eb
Willian Petri <wopetri11@gmail.com>
2020-07-25 12:55:20 -0300

    Migration, product e customer feitos

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -11,15 +11,19 @@ import {
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   customer: Customer;
 
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
